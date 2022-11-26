48d964151c4e317ea01d5d7fe5a9ae9d9ecb1ee3
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-11-26 22:11:14 +0100

    add stylings

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { FaAngleDoubleRight } from 'react-icons/fa';
+import { AiOutlineBars } from 'react-icons/ai';
 import logo from '../assets/logo.gif';
 import navbarItems from '../data';
 
@@ -11,11 +11,11 @@ const Navbar = () => {
       <div className="flex max-h-[90vh]">
         <div
           className={`${
-            open ? 'w-72' : 'w-20'
-          } h-screen  relative duration-300 `}
+            open ? 'w-56' : 'w-20'
+          } h-screen border-r-2 border-slate-300 bg-white relative duration-300 `}
         >
-          <FaAngleDoubleRight
-            className={`absolute cursor-pointer rounded-full -right-3 top-9 w-7 border-2 border-secondary ${
+          <AiOutlineBars
+            className={`absolute cursor-pointer -right-3 top-9 w-9 font-medium text-2xl ${
               !open && 'rotate-180'
             }`}
             onClick={() => setOpen(!open)}
@@ -27,23 +27,27 @@ const Navbar = () => {
           >
             <img src={logo} alt="logo" />
           </div>
-          <ul className="pt-6">
-            {navbarItems.map((item) => (
-              <li
-                key={item.name}
-                className={`text-gray-300 text:sm flex item-center gap-x-4 cursor-pointer p-2 hover:bg-slate-300 rounded-md ${
-                  item.gap ? 'mt-9' : 'mt-2'
-                }`}
-              >
-                <FaAngleDoubleRight />
-                <span
-                  className={`${!open && 'hidden'} origin-left duration-200`}
+          <nav>
+            <ul className="pt-6">
+              {navbarItems.map((item) => (
+                <li
+                  key={item.name}
+                  className={`text-white text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-gray-400 rounded-bl-lg ${
+                    item.gap ? 'mt-9' : 'mt-2'
+                  }`}
                 >
-                  {item.name}
-                </span>
-              </li>
-            ))}
-          </ul>
+                  <AiOutlineBars className="text-black font-extrabold text-xl" />
+                  <span
+                    className={`${
+                      !open && 'hidden'
+                    } origin-left duration-200 text-base font-extrabold text-center text-slate-800`}
+                  >
+                    {item.name}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </nav>
         </div>
         <div className="p-7 text-2xl font-semibold flex-1 h-screen">
           <h1>Homepage</h1>
