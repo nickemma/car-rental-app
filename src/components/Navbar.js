3a3808f3d4d5e5f8e50cef60bf1f1391d154265a
AACHOURMOHAMED <aachourmohameddeveloper@gmail.com>
2022-11-26 09:14:47 +0100

    Add navbar data

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,18 @@
 import { useState } from 'react';
 import { FaAngleDoubleRight } from 'react-icons/fa';
-import logo from '../assests/logo.gif';
+import logo from '../assets/logo.gif';
+import navbarItems from '../data';
 
 const Navbar = () => {
   const [open, setOpen] = useState(true);
 
   return (
     <>
-      <div className="flex">
+      <div className="flex max-h-[90vh]">
         <div
           className={`${
             open ? 'w-72' : 'w-20'
-          } h-screen bg-secondary relative duration-300 `}
+          } h-screen  relative duration-300 `}
         >
           <FaAngleDoubleRight
             className={`absolute cursor-pointer rounded-full -right-3 top-9 w-7 border-2 border-secondary ${
@@ -26,6 +27,19 @@ const Navbar = () => {
           >
             <img src={logo} alt="logo" />
           </div>
+          <div className="flex flex-col items-center">
+            {navbarItems.map((item) => (
+              <div
+                key={item.name}
+                className={`${
+                  open ? 'w-36' : 'w-20'
+                } h-10  flex items-center  cursor-pointer duration-300 hover:bg-primary`}
+              >
+                {item.name}
+              </div>
+            ))}
+          </div>
+
         </div>
         <div className="p-7 text-2xl font-semibold flex-1 h-screen">
           <h1>Homepage</h1>
---
+++ src/data/index.js
@@ -0,0 +1,16 @@
+const navbarItems = [
+  {
+    name: 'MODELS',
+    path: '/',
+  },
+  {
+    name: 'RESERVATIONS',
+    path: '/reservations',
+  },
+  {
+    name: 'BOOKING',
+    path: '/booking',
+  },
+];
+
+export default navbarItems;
