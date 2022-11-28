66b31d56f67d7e8ee04427e5bbd196a0f70b7327
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-11-28 23:31:24 +0100

    fixed eslint error

--- src/redux/reducers/carReducer.js
+++ src/redux/reducers/carReducer.js
@@ -1,32 +1,32 @@
-// import * as types from '../constants/carConstants';
+import * as types from '../constants/carConstants';
 
-// const initialState = {
-//   cars: [],
-//   car: {},
-//   loading: false,
-// };
+const initialState = {
+  cars: [],
+  car: {},
+  loading: false,
+};
 
-// const carReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case types.GET_CARS_REQUEST:
-//       return { ...state, loading: true };
-//     case types.GET_CARS_SUCCESS:
-//       return { ...state, cars: action.payload, loading: false };
-//     case types.GET_CARS_FAIL:
-//       return { ...state, loading: false };
-//     case types.DELETE_CAR_REQUEST:
-//       return { ...state, loading: true };
-//     case types.DELETE_CAR_SUCCESS:
-//       return {
-//         ...state,
-//         cars: state.cars.filter((car) => car.id !== action.payload),
-//         loading: false,
-//       };
-//     case types.DELETE_CAR_FAIL:
-//       return { ...state, loading: false };
-//     default:
-//       return state;
-//   }
-// };
+const carReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case types.GET_CARS_REQUEST:
+      return { ...state, loading: true };
+    case types.GET_CARS_SUCCESS:
+      return { ...state, cars: action.payload, loading: false };
+    case types.GET_CARS_FAIL:
+      return { ...state, loading: false };
+    case types.DELETE_CAR_REQUEST:
+      return { ...state, loading: true };
+    case types.DELETE_CAR_SUCCESS:
+      return {
+        ...state,
+        cars: state.cars.filter((car) => car.id !== action.payload),
+        loading: false,
+      };
+    case types.DELETE_CAR_FAIL:
+      return { ...state, loading: false };
+    default:
+      return state;
+  }
+};
 
-// export default carReducer;
+export default carReducer;
